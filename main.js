adb9bd35577050552ffcdde639bdf4bea17bfd22
anaYesi <anayesenia026@gmail.com>
2017-07-17 14:06:00 -0500

    anayesi

---
+++ main.js
@@ -0,0 +1,21 @@
+var peso = prompt("Ingrese su peso ");
+var masaTierra = peso /9.8;
+
+var pMercurio = masaTierra * 3.70;
+document.write("Tupeso en mercurio es : "+ pMercurio);
+var pVenus = masaTierra * 8.87;
+document.write("Tupeso en Venus es : "+ pVenus);
+var pMARTE = masaTierra * 3.71;
+document.write("Tupeso en Marte es : "+ pMARTE);
+var pJupiter = masaTierra * 23.12;
+document.write("Tupeso en Jupiter es : "+ pJupiter);
+var pSaturno = masaTierra * 8.96;
+document.write("Tupeso en Saturno es : "+ pSaturno);
+var pUrano = masaTierra * 8.69;
+document.write("Tupeso en Urano es : "+ pUrano);
+var pNeptuno = masaTierra * 11;
+document.write("Tupeso en Neptuno es : "+ pNeptuno);
+var pPluton = masaTierra * 0.81;
+document.write("Tupeso en Pluton es : "+ pPluton);
+ 
+ 
