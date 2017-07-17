53027a02f4d40bce0f47652fbcd0a16d3ed0c746
anaYesi <anayesenia026@gmail.com>
2017-07-17 14:17:19 -0500

    pesos en planetas

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-var peso = prompt("Ingrese su peso ");
+var nombre = prompt("Ingrese un nombre");
+var peso = prompt(nombre +"Ingrese su peso ");
 var masaTierra = peso /9.8;
 
 var pMercurio = masaTierra * 3.70;
-document.write("Tupeso en mercurio es : "+ pMercurio);
+document.write("Tupeso en mercurio es : "+ pMercurio.toFixed(2)+"</br>");
 var pVenus = masaTierra * 8.87;
-document.write("Tupeso en Venus es : "+ pVenus);
+document.write("Tupeso en Venus es : "+ pVenus.toFixed(2)+"</br>");
 var pMARTE = masaTierra * 3.71;
-document.write("Tupeso en Marte es : "+ pMARTE);
+document.write("Tupeso en Marte es : "+ pMARTE.toFixed(2)+"</br>");
 var pJupiter = masaTierra * 23.12;
-document.write("Tupeso en Jupiter es : "+ pJupiter);
+document.write("Tupeso en Jupiter es : "+ pJupiter.toFixed(2)+"</br>");
 var pSaturno = masaTierra * 8.96;
-document.write("Tupeso en Saturno es : "+ pSaturno);
+document.write("Tupeso en Saturno es : "+ pSaturno.toFixed(2)+"</br>");
 var pUrano = masaTierra * 8.69;
-document.write("Tupeso en Urano es : "+ pUrano);
+document.write("Tupeso en Urano es : "+ pUrano.toFixed(2)+"</br>");
 var pNeptuno = masaTierra * 11;
-document.write("Tupeso en Neptuno es : "+ pNeptuno);
+document.write("Tupeso en Neptuno es : "+ pNeptuno.toFixed(2)+"</br>");
 var pPluton = masaTierra * 0.81;
-document.write("Tupeso en Pluton es : "+ pPluton);
+document.write("Tupeso en Pluton es : "+ pPluton.toFixed(2)+"</br>");
  
  
